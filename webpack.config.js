7daaa585e117c9b2cb4fc8b7224279373da70423
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-12-10 14:29:43 +0100

    Add webpack/babel

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+let path = require('path')
+
+let src_path  = path.resolve('./src')
+let dist_path = path.resolve('./dist')
+
+let output_filename = "lw.svg-parser.js"
+let output_library  = "SVGParser"
+
+module.exports = {
+    debug: true,
+    context: src_path,
+    devtool: "source-map",
+    entry: [
+        "./parser.js"
+    ],
+    output: {
+        path: dist_path,
+        filename: output_filename,
+        library: output_library,
+        libraryTarget: "umd",
+        umdNamedDefine: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ["babel-loader"]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: dist_path
+    }
+};
